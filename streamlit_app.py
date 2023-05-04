8920247f6d7c897577adcfb8b098c4dc22ed197f
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-05-04 16:38:57 +0500

    checking streamlit docs

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,14 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+st.text_input("my name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
